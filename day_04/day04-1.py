c1b25b3d4aa3b061749e91215b96a349b96efc9e
Alex Roosma <alex.roosma@outlook.com>
2023-12-04 20:25:48 -0500

    day 04 - pt 1 solution

--- day_04/day04-1.py
+++ day_04/day04-1.py
@@ -1,11 +1,34 @@
-DEBUG = True
+DEBUG = False
 
 if DEBUG:
     file = open("day_04_input_test.txt", "r")
 else:
     file = open("day_04_input.txt", "r")
 original_data = file.read().strip().split("\n")
-
-
-
 file.close()
+
+total = 0
+
+for line in original_data:
+    count_wins = 0
+    half_1, half_2 = line.split('|')
+
+    winners = []
+    scratch_nums = []
+
+    for _t in half_1.split(":")[1].strip().split(' '):
+        if _t.isdigit():
+            winners.append(int(_t))
+
+    for _t in half_2.strip().split(' '):
+        if _t.isdigit():
+            scratch_nums.append(int(_t))
+
+    for _num in scratch_nums:
+        if _num in winners:
+            count_wins += 1
+    
+    if count_wins >= 1:
+        total += 2 ** (count_wins-1)
+
+print(total)
